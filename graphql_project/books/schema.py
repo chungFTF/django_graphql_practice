bdbba70a84aaac1b8fbf422803078d00ca4d9c10
chungFTF <stephaniechung0729@gmail.com>
2025-03-02 00:15:55 +0800

    add gql schema

--- graphql_project/books/schema.py
+++ graphql_project/books/schema.py
@@ -6,3 +6,13 @@ class BooksType(DjangoObjectType):
     class Meta:
         model = Books
         fields = ("id", "title", "excerpt")
+
+
+class Query(graphene.ObjectType):
+    all_books = graphene.List(BooksType)
+
+    def resolve_all_books(root, info):
+        return Books.objects.all()
+
+
+schema = graphene.Schema(query=Query)
